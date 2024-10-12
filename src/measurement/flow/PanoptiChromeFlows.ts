6dd1e28594599d5eb8f6ebdabd9a6d7359b3948c
Samuele Casarin <eleumasc@gmail.com>
2024-10-12 10:55:12 -0400

    Instrument PanoptiChromeFlows to find more errors

--- src/measurement/flow/PanoptiChromeFlows.ts
+++ src/measurement/flow/PanoptiChromeFlows.ts
@@ -9,43 +9,54 @@ export const getPanoptiChromeFlows = (rawFlows: any): DetailedFlow[] => {
   for (const match of rawFlows.matchAll(
     /^(ORIGIN)@("[^"]*")$|^(SINKPC)\$\{Window\}:("fetch"):("[^"]+")(?:\{.*\})?\n^(LEAK)\$[0-9]+:\{Window\}:("fetch"):(.*)$/gm
   )) {
-    if (match[1]) {
-      // Set origin
-      currentOrigin = parseData(match[2]);
-    } else {
-      // Build flow
-      assert(match[3]);
-      assert(typeof currentOrigin === "string");
-      const targetUrl = new URL(parseData(match[5]), currentOrigin).toString();
-      const sources = match[8].split(",");
+    try {
+      if (match[1]) {
+        // Set origin
+        currentOrigin = parseData(match[2]);
+      } else {
+        // Build flow
+        assert(match[3]);
+        assert(typeof currentOrigin === "string");
+        const targetUrl = new URL(
+          parseData(match[5]),
+          currentOrigin
+        ).toString();
+        const sources = match[8].split(",");
 
-      if (sources.includes("Document.cookie")) {
-        flows = [
-          ...flows,
-          {
-            sink: { type: "network", targetUrl },
-            source: { type: "cookie" },
-          },
-        ];
-      }
+        if (sources.includes("Document.cookie")) {
+          flows = [
+            ...flows,
+            {
+              sink: { type: "network", targetUrl },
+              source: { type: "cookie" },
+            },
+          ];
+        }
 
-      if (
-        sources.includes("Storage.getItem") &&
-        sources.includes("Window.localStorage")
-      ) {
-        flows = [
-          ...flows,
-          {
-            sink: { type: "network", targetUrl },
-            source: { type: "localStorage" },
-          },
-        ];
+        if (
+          sources.includes("Storage.getItem") &&
+          sources.includes("Window.localStorage")
+        ) {
+          flows = [
+            ...flows,
+            {
+              sink: { type: "network", targetUrl },
+              source: { type: "localStorage" },
+            },
+          ];
+        }
       }
-    }
+    } catch {}
   }
 
   return flows;
 };
 
-const parseData = (data: string): string =>
-  JSON.parse(data.replace("\\:", ":"));
+const parseData = (data: string): string => {
+  try {
+    return JSON.parse(data.replace("\\:", ":"));
+  } catch (e) {
+    console.error(`Cannot parse: ${data}`);
+    throw e;
+  }
+};
